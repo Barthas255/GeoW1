584429c6c2a13c2d7eadc95db7bf14476c7741bd
Bartłomiej Jurga <55412427+Barthas255@users.noreply.github.com>
2021-12-08 02:22:22 +0100

    sfery podkładowe
    ostatnie poprawki w kodzie przed napisaniem sprawozdania

--- projekt2_astronomia/projekt2_astro.py
+++ projekt2_astronomia/projekt2_astro.py
@@ -1,10 +1,9 @@
 from astropy.time import Time
-import numpy as np
 from astropy.coordinates import Angle
 from astropy import units as u
 import matplotlib.pyplot as pl
 import matplotlib
-from mpl_toolkits.mplot3d import Axes3D
+import numpy as np
 
 #Obserwatorzy
 obs1 = (52.232222, 21.008333) # Warszawa fi,lambda
@@ -26,7 +25,6 @@ def kat_godz(lamb,alfa,h):
     S = ut1*15 + lamb + gmst
     #obliczenie kąta godzinnego(w stopniach)
     t = S - alfa * 15
-    #print("godzina = ", h,"kat godzinny = ",t, "czas gwiazdowy = ", S)
     return t
 
 
@@ -49,8 +47,8 @@ def az_and_zenit(fi, delta, t):
     elif licznik < 0: Az += 360
     if Az > 360:
         Az -= 360
-    #print("azymut =",Az,"zenit=", z)
     return z, Az
+
 
 #Zamiana na xyz
 def x_y_z(z, Az, r = 1):
@@ -62,7 +60,7 @@ def x_y_z(z, Az, r = 1):
     return x, y, z
 
 
-#przesuwanie lewej górnej krawędzi wykresu do punktu x y
+#przesuwanie utworzonego wykresu do punktu x y
 def move_figure(f, x, y):
     backend = matplotlib.get_backend()
     if backend == 'TkAgg':
@@ -72,8 +70,8 @@ def move_figure(f, x, y):
     else:
         f.canvas.manager.window.move(x, y)
 
-#Wykresy
 
+#Wykresy
 x = []
 y = []
 z = []
@@ -119,6 +117,8 @@ for i in range(1, 25):
     y2.append(xyz2[1])
     z2.append(xyz2[2])
 
+
+#Wykres wysokości od czasu
 def draw_2D(czas, h, clr, ls_type):
     fig = pl.figure("Wysokość w czasie", figsize=(7,5))
     pl.plot(czas, h, color=clr, ls = ls_type)
@@ -126,44 +126,61 @@ def draw_2D(czas, h, clr, ls_type):
     pl.xlabel("czas lokalny")
     pl.ylabel("wysokość")
     pl.xticks(np.arange(min(czas), max(czas) + 2, 1))
+    pl.gca().spines['top'].set_visible(False)
+    pl.gca().spines['right'].set_visible(False)
     return fig
 
-#wykres 2D wys(h)
+
+#wykres 2D h(t)
 czas = [i for i in range(24)] #posłuży jako oś X
 fig = draw_2D(czas,h,"blue",'-')
 draw_2D(czas,h1,"green",'--')
 draw_2D(czas,h2,"red",':')
 
 
-
-
 #wykres 3D xyz
+
+#Płn półkula
 fig2 = pl.figure("Wykres xyz Warszawa", figsize=(5,4))
 ax2 = fig2.add_subplot(111,projection='3d')
-ax2.scatter(x,y,z,c='blue',marker='o')
+ax2.scatter(x,y,z,c='blue',marker='o', zorder=2)
 ax2.set_xlabel('X')
 ax2.set_ylabel('Y')
 ax2.set_zlabel('Z')
 
-
+#Równik
 fig3 = pl.figure("Wykres xyz Dżakarta (fi = 0)", figsize=(5,4))
 ax3 = fig3.add_subplot(111,projection='3d')
-ax3.scatter(x1,y1,z1,c='green',marker='o')
+ax3.scatter(x1,y1,z1,c='green',marker='o', zorder=2)
 ax3.set_xlabel('X')
 ax3.set_ylabel('Y')
 ax3.set_zlabel('Z')
 
-
+#Płd półkula
 fig4 = pl.figure("Wykres xyz Canberra", figsize=(5,4))
 ax4 = fig4.add_subplot(111,projection='3d')
-ax4.scatter(x2,y2,z2,c='red',marker='o')
+ax4.scatter(x2,y2,z2,c='red',marker='o', zorder=2)
 ax4.set_xlabel('X')
 ax4.set_ylabel('Y')
 ax4.set_zlabel('Z')
 
+#upewnienie sie, ze wykres na równiku jest w notacji naukowej
 ax3.ticklabel_format(axis="both", style="sci",scilimits = (0,0))
+
+#przesunięcie figur na ekranie
 move_figure(fig, 700, 5)
-move_figure(fig2, 100, 500)
-move_figure(fig3, 700, 500)
-move_figure(fig4, 1300, 500)
+move_figure(fig2, 100, 550)
+move_figure(fig3, 700, 550)
+move_figure(fig4, 1300, 550)
+
+#sfery referencyjne
+r = 0.95
+u, v = np.mgrid[0:2 * np.pi:30j, 0:np.pi:20j]
+a = r*np.cos(u) * np.sin(v)
+b = r*np.sin(u) * np.sin(v)
+c = r*np.cos(v)
+ax2.plot_surface(a, b, c, cmap = 'winter', alpha=0.5, zorder=1)
+ax3.plot_surface(a, b, c, cmap = 'summer', alpha=0.5, zorder=1)
+ax4.plot_surface(a, b, c, cmap = 'autumn', alpha=0.5, zorder=1)
+
 pl.show()
